476222a1b0d10f9405954240b71369683ed09f08
Tarcisio Coutinho <tcs5cin@gmail.com>
2012-10-31 19:16:02 -0300

    add new invocation event method

--- src/main/java/br/ufpe/cin/dsoa/event/InvocationEvent.java
+++ src/main/java/br/ufpe/cin/dsoa/event/InvocationEvent.java
@@ -1,10 +1,17 @@
 package br.ufpe.cin.dsoa.event;
 
+import java.util.List;
+
 
 public class InvocationEvent {
 
 	public InvocationEvent(Request request, Response response) {
 		// TODO Auto-generated constructor stub
+	}
+
+	public List<String> getMonitoredAttributes() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
