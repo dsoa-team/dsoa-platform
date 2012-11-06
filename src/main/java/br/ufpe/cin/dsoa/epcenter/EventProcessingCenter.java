72b093080b1e225a4f31a0efe8c1068761718b96
Tarcisio Coutinho <tcs5cin@gmail.com>
2012-11-06 17:28:12 -0300

    move epcenter to platform

--- src/main/java/br/ufpe/cin/dsoa/epcenter/EventProcessingCenter.java
+++ src/main/java/br/ufpe/cin/dsoa/epcenter/EventProcessingCenter.java
@@ -5,13 +5,11 @@ import java.util.Map;
 
 public interface EventProcessingCenter {
 
-	@SuppressWarnings("rawtypes")
-	public void defineEvent(Class event);
+	public void defineEvent(Class<?> event);
 
 	public void publishEvent(Object event);
 
-	@SuppressWarnings("rawtypes")
-	public void publishEvent(Map event, String eventName);
+	public void publishEvent(Map<?,?> event, String eventName);
 
 	public void defineStatement(String name, String statement, List<String> userObject);
 	
